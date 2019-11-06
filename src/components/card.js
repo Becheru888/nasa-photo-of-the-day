a3712ba59dede0f05265fc31b6ccf9604f82684f
Becheru888 <becheru93@gmail.com>
2019-11-06 15:55:14 +0000

    Searching by date added :)

--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
 function Card(props) {
-  console.log(props.data);
-
-  if (!props.data){
-    return (<h1>Please wait...</h1>)
-  }else{
+  if (!props.data) {
+    return <h3>Please wait...</h3>;
+  } else {
     return (
-        <div>
-          <img src={props.data.url} alt="space-img" />
-          <div className="details">
-              <h1>{props.data.title}</h1>
-              <p>{props.data.date}</p>
-              <p>{props.data.explenation}</p>
-          </div>
+      <StyledCard>
+        <img src={props.data.url} alt="space-img" />
+        <div className="details">
+          <h1>{props.data.title}</h1>
+          <p>{props.data.date}</p>
+          <p>{props.data.explenation}</p>
         </div>
-      );
+      </StyledCard>
+    );
   }
- 
 }
 
 export default Card;
+
+const StyledCard = styled.div`
+  img {
+    width: 100%;
+    height:300px;
+  }
+  div {
+      height:100%;
+  }
+`;
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "./card";
+import styled from "styled-components";
 
 function Wrapper() {
   const [nasaData, setData] = useState({});
-
+  const [inputDate, setInput] = useState('')
+  
+  const api = "https://api.nasa.gov/planetary/apod?api_key=dCT5VofNFrUAI6zPXdKUHtAOKgSyRyzhOR6WanbC"
   useEffect(() => {
     axios
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=dCT5VofNFrUAI6zPXdKUHtAOKgSyRyzhOR6WanbC"
-      )
+      .get(api)
       .then(res => {
         setData(res.data);
       })
@@ -19,10 +20,29 @@ function Wrapper() {
   }, []);
 
   return (
-    <>
-      <Card data={nasaData} />;
-    </>
+    <CardWrapper>
+      <Card data={nasaData} />
+      <form onSubmit={e => {
+          e.preventDefault();
+          axios.get(`https://api.nasa.gov/planetary/apod?api_key=dCT5VofNFrUAI6zPXdKUHtAOKgSyRyzhOR6WanbC&date=${inputDate}`).then(res => {
+              setData(res.data)
+          })
+      }}>
+      <input type='text' onChange={e => {
+          setInput(e.target.value)
+      }}/>
+      <button type='submit'>Find By Date</button>
+      </form>
+      
+    </CardWrapper>
   );
 }
 
 export default Wrapper;
+
+const CardWrapper = styled.div`
+  margin: 0 auto;
+  width:30%;  
+  border: 2px dashed red;
+  padding:20px;
+`;
